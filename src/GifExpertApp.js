77376ba90c415cf93d4840a29f8db8a2bf2accc1
BestoWaifu02 <BestoWaifu02@gmail.com>
2020-07-13 22:15:43 -0500

    comentarios retidados

--- src/GifExpertApp.js
+++ src/GifExpertApp.js
@@ -1,11 +1,7 @@
 import React, { useState } from 'react'
 import { AddCategory } from './components/AddCategory';
 import { GifGrid } from './components/GifGrid';
-
-export const GifExpertApp = () => {
-    
-    const [categories, setCategories] = useState(['Akame Ga Kill']);
- /*  const categories=['samurai X','One punch','SAO']
+/*  const categories=['samurai X','One punch','SAO']
   const [categories, setcategories] = useState([
     "samurai X",
     "One punch",
@@ -22,6 +18,10 @@ export const GifExpertApp = () => {
     //El set categories al ser un callback lo puedes manejar de esta forma
     setcategories(cats=>[...cats,'Tokyo Ghoul'])
 }; */
+export const GifExpertApp = () => {
+    
+    const [categories, setCategories] = useState(['Akame Ga Kill']);
+ 
     return (
         <>
             <h2>App By BestoWaifu02 (Diego Robledo Mendoza)</h2>
